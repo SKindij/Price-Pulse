f0a92a703360ef60099b4e5e52d22c8313ce4bf2
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-12-06 22:49:05 +0200

    init: set up project structure and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-     
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
+    <main className={styles.main}>  
+      <p>
+        Get started by editing&nbsp;
+        <code className={styles.code}>app/page.tsx</code>
+      </p>
        
-          <p>Find in-depth information about Next.js features and API.</p>
-       
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-       
+      <Container>
+        <Typography variant="h2">Hello NEXT World</Typography>
+      </Container>
+
+
+        <p>Find in-depth information about Next.js features and API.</p>
     </main>
   )
 }
