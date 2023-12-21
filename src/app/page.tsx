0733c1e17884648f0e591190705903ee7399be62
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-12-22 00:04:07 +0200

    feat: integrate third-party library for image processing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,35 @@ import styles from './page.module.css';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Image from 'next/image';
+import Grid from '@mui/material/Grid';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Price-Pulse: твій помічник у виборі товарів, рецептів та коктейлів.</h1>
+      <Typography variant="h1">
+        Твій помічник у виборі товарів з найкращою ціною, рецептів та коктейлів.
+      </Typography>
       {/*  */}
-
-      <Container>
-        {/*  */}
-        <Image
-          src="/images/main-page.jpg"
-          width={500} height={500}
-          alt="Picture of Price-Pulse"
-        />
-      </Container>
-
+      <Grid container spacing={2} className={styles.gridContainer}>
+        <Grid item xs={12} md={4}>
+          <Image alt="Coctails Palitre"
+            src="/images/cocktails-palitre.jpg"
+            width={400} height={400}
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Image alt="Picture of Price-Pulse"
+            src="/images/main-page.jpg"
+            width={400} height={400}
+          />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Image alt="Receipes Palitre"
+            src="/images/receipes-palitre.jpg"
+            width={400} height={400}
+          />
+        </Grid>
+      </Grid>
     </main>
   );
 }
--- src/views/Categories/CocktailsCardGrid.tsx
+++ src/views/Categories/CocktailsCardGrid.tsx
@@ -6,12 +6,12 @@ import CoctailReviewCard from '@/views/Categories/CoctailReviewCard';
 import { cocktailsData } from '@/seeder/placeholder-data';
 
 export default function CocktailsCardGrid() {
-  return (  
+  return (
     <Grid container spacing={{ xs:3, sm:2, md:3 }} >
 	  {/* spacing:{spacing between elements in container} */}
-      { cocktailsData.map((cocktail) => (      
-		<Grid item xs={12} sm={6} md={4} lg={3} key={cocktail.cocktailID}>
-          {/* item:{column width for different screens} */} 
+      { cocktailsData.map((cocktail) => (
+		  <Grid item xs={12} sm={6} md={4} lg={3} key={cocktail.cocktailID}>
+          {/* item:{column width for different screens} */}
 		  <CoctailReviewCard cocktail={cocktail} />
         </Grid>
       )) }
