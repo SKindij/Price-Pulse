b6039b1d1bcb2780f4db9fb21139cf2955e22a38
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-12-22 19:21:56 +0200

    style: standardize CSS styles for cohesive design

--- src/views/ThemeRegistry/theme.ts
+++ src/views/ThemeRegistry/theme.ts
@@ -2,6 +2,8 @@
 import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
 
+import { purple } from '@mui/material/colors';
+
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
   subsets: ['latin'],
@@ -15,18 +17,28 @@ const theme = createTheme({
       main: '#f0c000', // mainly orange color
     },
     secondary: {
-      main: '#208080', // color of sea wave
+      main: purple[500], // purple shade
     },
   },
   typography: {
     fontFamily: roboto.style.fontFamily,
     h1: {
-      fontSize: '1.6rem',
-      fontWeight: 400,
+      fontSize: '1.8rem',
+      fontWeight: 800,
       margin: '1rem 0',
     },
     h2: {
+      fontSize: '1.6rem',
+      fontWeight: 600,
+      margin: '1rem 0',
+    },
+	h3: {
       fontSize: '1.4rem',
+      fontWeight: 400,
+      margin: '1rem 0',
+    },
+	h4: {
+      fontSize: '1.2rem',
       fontWeight: 400,
       margin: '1rem 0',
     },
