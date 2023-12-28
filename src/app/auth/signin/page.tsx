7f3e4d79a14dd06562562a6005b36b5fede49cdd
”SerhiiKindij” <serhii.kindij@gmail.com>
2023-12-28 20:07:51 +0200

    feat: introduce user authentication feature

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,12 @@
+// @/app/auth/signin/page.tsx
+import SignInForm from '@/views/SignInForm';
+
+
+export default function SignInPage() {
+  return (
+    <div>
+      {/*  */}
+      <SignInForm />
+    </div>
+  );
+}
---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,12 @@
+// @/app/auth/signup/page.tsx
+import SignUpForm from '@/views/SignUpForm';
+
+
+export default function SigUpPage() {
+  return (
+    <div>
+      {/*  */}
+      <SignUpForm />
+    </div>
+  );
+}
