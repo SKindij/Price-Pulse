e5206acb46759b870a16783046a1868486398532
”SerhiiKindij” <serhii.kindij@gmail.com>
2024-01-11 23:04:21 +0200

    refactor: enhance code readability

--- src/seeder/seed.js
+++ src/seeder/seed.js
@@ -44,8 +44,8 @@ async function seedCategories(client) {
     const insertedCategories = await Promise.all(
       drinkCategoriesData.map(
         (category) => client.sql`
-        INSERT INTO drink_categories (drink_category)
-        VALUES (${category})
+        INSERT INTO drink_categories (category_id, category_name)
+        VALUES (${category.categoryID}, ${category.categoryName})
         ON CONFLICT DO NOTHING;
       `,
       ),
@@ -56,31 +56,6 @@ async function seedCategories(client) {
     throw error;
   }
 }
-// ----- "retail_chains" table -----
-async function seedRetailChains(client) {
-  try {
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS retail_chains (
-        retail_chain_id SMALLSERIAL PRIMARY KEY,
-        retail_chain_name VARCHAR(20) NOT NULL UNIQUE
-      );
-    `;
-    console.log('Created "retail_chains" table');
-    const insertedChains = await Promise.all(
-      retailChainsData.map(
-        (retail) => client.sql`
-        INSERT INTO retail_chains (retail_chain_name)
-        VALUES (${retail})
-        ON CONFLICT DO NOTHING;
-      `,
-      ),
-    );
-    console.log(`Seeded ${insertedChains.length} chains`);
-  } catch (error) {
-    console.error('Error seeding chains:', error);
-    throw error;
-  }
-}
 // ----- "beverages_data" table -----
 async function seedBeverages(client) {
   try {
@@ -88,8 +63,14 @@ async function seedBeverages(client) {
       CREATE TABLE IF NOT EXISTS beverages_data (
         beverage_id SERIAL PRIMARY KEY,
         beverage_title VARCHAR(40) NOT NULL UNIQUE,
-        category_id SMALLINT REFERENCES drink_categories(drink_id),
+        category_id SMALLINT REFERENCES drink_categories(category_id),
         beverage_volume FLOAT NOT NULL,
+        auchan_price SMALLINT NOT NULL,
+        novus_price SMALLINT NOT NULL,
+        silpo_price SMALLINT NOT NULL,
+        atb_price SMALLINT NOT NULL,
+        rozetka_price SMALLINT NOT NULL,
+        last_updated DATE,
         beverage_in_wish BOOLEAN NOT NULL,
         beverage_ratings SMALLINT NOT NULL,
         country_id SMALLINT REFERENCES countries(country_id),
@@ -103,6 +84,7 @@ async function seedBeverages(client) {
       beveragesData.map(
         (beverage) => client.sql`
         INSERT INTO beverages_data (beverage_id, beverage_title, category_id, beverage_volume, 
+          auchan_price, novus_price, silpo_price, atb_price, rozetka_price, last_updated,
           beverage_in_wish, beverage_ratings, country_id, beverage_description, beverage_image_url)
         VALUES (${beverage.beverageId}, ${beverage.title}, ${beverage.category}, ${beverage.volume}, 
           ${beverage.inWish}, ${beverage.ratings}, ${beverage.countryID}, ${beverage.description}, ${beverage.imageUrl})
@@ -116,168 +98,14 @@ async function seedBeverages(client) {
     throw error;
   }
 }
-// Insert data into the "beverage_prices" table
-async function seedBeveragePrices(client) {
-  try {
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS beverage_prices (
-        beverage_id SMALLINT REFERENCES beverages_data(beverage_id),
-        retail_chain_id SMALLINT REFERENCES retail_chains(retail_chain_id),
-        price numeric NOT NULL,
-        last_updated DATE
-      );
-    `;
-    console.log('Created "beverage_prices" table');
-    for (const beverage of beveragesData) {
-      const prices = beverage.prices;
-      const beverageId = beverage.beverageId;
-      // let's go through the price array
-      for (const retailChainId in prices) {
-        const price = prices[retailChainId].price;
-        const lastUpdated = prices[retailChainId].lastUpdated;
-        // Insert price into beverage_prices table
-        await client.query({
-          text: `
-            INSERT INTO beverage_prices (beverage_id, retail_chain_id, price, last_updated)
-            VALUES ($1, $2, $3, $4)
-            ON CONFLICT (beverage_id, retail_chain_id) DO NOTHING;
-          `,
-          values: [beverageId, retailChainId, price, lastUpdated],
-        });
-      }
-    }
-    console.log('Beverage prices seeded successfully');
-  } catch (error) {
-    console.error('Error seeding beverage prices:', error);
-    throw error;
-  }
-}
-// fill in table with ingredients for cocktails
-async function seedCocktailIngredients(client) {
-  try {
-    await client.query(`
-      CREATE TABLE IF NOT EXISTS cocktail_ingredients (
-        ingredient_id SMALLSERIAL PRIMARY KEY,
-        ingredient_name VARCHAR(50) UNIQUE
-      )
-    `);
-    console.log('Created "cocktail_ingredients" table');
-    for (const ingredient of cocktailIngredients) {
-      await client.query(
-        'INSERT INTO cocktail_ingredients (ingredient_name) VALUES ($1) ON CONFLICT DO NOTHING',
-        [ingredient]
-      );
-    }
-    console.log('The table has been successfully populated with data.');
-  } catch (err) {
-    console.error('An error occurred while creating a table or filling it with data:', err);
-  }
-}
-// Insert data into the "drink_categories" table
-async function seedGlassTypes(client) {
-  try {
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS glass_types (
-        glass_id SMALLSERIAL PRIMARY KEY,
-        glass_type VARCHAR(20) NOT NULL UNIQUE
-      );
-    `;
-    console.log('Created "glass_types" table');
-    const insertedTypes = await Promise.all(
-      glassTypes.map(
-        (type) => client.sql`
-        INSERT INTO glass_types (glass_type)
-        VALUES (${type})
-        ON CONFLICT DO NOTHING;
-      `,
-      ),
-    );
-    console.log(`Seeded ${insertedTypes.length} types`);
-  } catch (error) {
-    console.error('Error seeding glass_types:', error);
-    throw error;
-  }
-}
-// Insert data into the "cocktails_data" table
-async function seedCocktails(client) {
-  try {
-    const createTable = await client.sql`
-    CREATE TABLE IF NOT EXISTS cocktails_data (
-      cocktail_id SMALLSERIAL PRIMARY KEY,
-      cocktail_title VARCHAR(50) NOT NULL UNIQUE,
-      image_url VARCHAR(120),
-      alcoholic BOOLEAN,
-      glass_type_id SMALLINT REFERENCES glass_types(glass_id),
-      instructions TEXT[]
-    ); 
-    `;
-    console.log('Created "cocktails_data" table');
-    const insertedCocktails = await Promise.all(
-      cocktailsData.map(
-        (cocktail) => client.sql`
-        INSERT INTO cocktails_data ( cocktail_id, cocktail_title, image_url,
-          alcoholic, glass_type_id, instructions )
-        VALUES (${cocktail.cocktailID}, ${cocktail.cocktailTitle}, ${cocktail.imageUrl},
-          ${cocktail.alcoholic}, ${cocktail.glassTypeID}, ${cocktail.instructions} )
-          ON CONFLICT (cocktail_title) DO NOTHING;
-      `,
-      ),
-    );
-    console.log(`Seeded ${insertedCocktails.length} cocktails`);
-  } catch (error) {
-    console.error('Error seeding Cocktails:', error);
-    throw error;
-  }
-}
-// fill in table with ingredients with volumes
-async function seedIngredientsVolume(client) {
-  try {
-    console.log('Running seedIngredientsVolume function.');
-    const createTable = await client.sql`
-    CREATE TABLE IF NOT EXISTS cocktails_ingredients_volume (
-      cocktail_id SMALLINT REFERENCES cocktails_data(cocktail_id),
-      ingredient_id SMALLINT REFERENCES cocktail_ingredients(ingredient_id),
-      volume VARCHAR(50),
-      PRIMARY KEY (cocktail_id, ingredient_id)
-    );
-    `;
-    console.log('Created "cocktails_ingredients_volume" table');
-    for (const cocktail of cocktailsData) {
-      const { cocktailID, ingredients } = cocktail;
-      // Convert ingredients object to an array of objects
-      const ingredientsArray = Object.entries(ingredients).map(([ingredientID, volume]) => ({
-        ingredientID: parseInt(ingredientID), // Convert to number if needed
-        volume,
-      }));
-      for (const ingredient of ingredientsArray) {
-        const { ingredientID, volume } = ingredient;
 
-        await client.sql`
-          INSERT INTO cocktails_ingredients_volume (cocktail_id, ingredient_id, volume)
-          VALUES (${cocktailID}, ${ingredientID}, ${volume})
-          ON CONFLICT DO NOTHING;
-        `;
-      }
-    }
-    console.log('Cocktail ingredients volumes seeded successfully');
-  } catch (error) {
-    console.error('Error seeding cocktail ingredients volumes:', error);
-    throw error;
-  }
-}
 // start the process of initial filling of the database
 async function main() {
   console.log('Підключаємося до Vercel PosgreSQL...');
   const client = await db.connect();
-  // await seedCountries(client);
-  // await seedCategories(client);
-  // await seedRetailChains(client);
-  // await seedBeverages(client);
-  // await seedBeveragePrices(client);
-  // await seedCocktailIngredients(client);
-  // await seedGlassTypes(client);
-  // await seedCocktails(client);
-  // seedIngredientsVolume(client)
+   await seedCountries(client);
+   await seedCategories(client);
+   await seedBeverages(client);
   await client.end();
 }
 main().catch((err) => {
